ae76cb8b2659e4f6db59afc54d0b0bff5a0e8dd1
Jiaqi <jiaqiviolin@gmail.com>
2026-03-05 19:54:20 -0700

    adding live face recognition with frame

--- face-recognition-frame.py
+++ face-recognition-frame.py
@@ -4,7 +4,7 @@ import os, sys
 import numpy as np
 import math
 
-# calculate acuracy percentage of faces, and display it on the screen
+# 1: calculate acuracy percentage of faces, and display it on the screen
 def face_confidence(face_distance, match_threshold=0.6):
     range = (1.0-match_threshold)
     linear_val = (1.0 - face_distance) / (range * 2.0)
@@ -44,10 +44,66 @@ class FaceRecognition:
                 continue
 
         print(" ")
-        print(self.known_face_names)
+        print(self.known_face_names)    # now the images in folder 'Jiaqi' are initialized
         print(" ")
         
 
 
+# 2: 
+    def run_recognition(self):
+        video_capture = cv2.VideoCapture(0)     # 你就一个camera， 0 是 index of camera
+        if not video_capture.isOpened():
+            print("Video source not found.")
+
+        while True:
+            ret, frame = video_capture.read() # weeither the fram is success or not, if there is no frame to process, ret will turn False!
+            
+            if self.process_currebt_frame:
+                size_frame = cv2.resize(frame, (0,0), fx=0.25, fy=0.25)
+                # rgb_size_frame = np.ascontiguousarray(size_frame[:, :, ::-1])     # Color Flip (Swaps BGR to RGB )
+                rgb_size_frame = cv2.cvtColor(size_frame, cv2.COLOR_BGR2RGB)        # new way of flip
+# locate all faces in the current frame:
+                self.face_locations = face_recognition.face_locations(rgb_size_frame)
+                self.face_encodings = face_recognition.face_encodings(rgb_size_frame, self.face_locations)
+                self.face_names = []
+                for face_encoding in self.face_encodings:
+                    matche = face_recognition.compare_faces(self.face_encodings, face_encoding)
+                    face_distances = face_recognition.face_distance(self.face_encodings, face_encoding)
+                    name = 'Unknown'
+                    confidence = 'Unknown'
+
+                    face_distances = face_recognition.face_distance(self.face_encodings, face_encoding)
+                    best_match_index = np.argmin(face_distances) # Finds the smallest distance
+
+                    if matche[best_match_index]:
+                        name = self.known_face_names
+                        confidence = face_confidence(face_distances[best_match_index])
+                    self.face_names.append(f'{name}({confidence})')
+
+            self.process_currebt_frame = not self.process_currebt_frame
+
+            for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
+                top *=4
+                right *= 4
+                bottom *= 4
+                left *=4
+
+                cv2.rectangle(frame, (left, top), (right,bottom), (0, 0, 255))
+                cv2.rectangle(frame, (left, bottom-35), (right,bottom), (0, 0, 255))
+                cv2.putText(frame, name, (left+6, bottom-6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
+            
+
+            cv2.imshow('Face Recognition', frame)
+
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+        video_capture.release()
+        cv2.destroyAllWindows()
+
+
+
+
 if __name__ == '__main__':
     fr = FaceRecognition()
+    fr.run_recognition()  # <--- You must call this to open the window!
